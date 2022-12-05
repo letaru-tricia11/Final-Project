c8a3f024c735979ce685db965f932a2b52281f7b
letaru-tricia11 <tricialetaru7@gmail.com>
2022-12-05 13:37:58 +0300

    statistics Q&A navbar

--- hac.js
+++ hac.js
@@ -1,3 +1,15 @@
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toogle('window-scroll',window.scrollY>100)
 })
+
+const faqs=document.querySelectorAll('.faq');
+faqs.forEach(faq =>{
+    faq.addEventListener('click,'(), {
+    faq,classList,toogle("open");
+    })
+})
+menuBtn.addEventListener('click',()=>{
+    menu.style.display="flex";
+    
+    closeBtn
+})
